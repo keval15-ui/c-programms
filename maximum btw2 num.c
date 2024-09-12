36fb2086c3a4f638f017979adae3d79d22cd0da0
keval15-ui <kevalsolankure@gmail.com>
2024-09-12 10:00:36 +0530

    Create maximum btw2 num.c

---
+++ maximum btw2 num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main()
+ {
+    int num1,num2;
+    printf("Enter value of num1:");
+    scanf("%d",&num1);
+    printf("Enter value of num2:");
+    scanf("%d",&num2);
+    
+    if (num1>num2){
+        printf("num1 is greater then num2");
+    }
+      else{
+          printf("num1 is not greater than num2");
+          
+      }
+      return 0;
+ }
