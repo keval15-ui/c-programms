c8c42397824cec3318c2f10030f77b66297312d7
keval15-ui <kevalsolankure@gmail.com>
2024-09-12 09:21:35 +0530

    Create add, sub,multi,divi.c

---
+++ add, sub,multi,divi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ int main()
+ {
+     int a,b,add,sub,multi,divi;
+     printf("Enter a value of a :");
+     scanf("%d",&a);
+     printf("Enter a value of b:");
+     scanf("%d",&b);
+     add=a+b;
+     sub=a-b;
+     multi=a*b;
+     divi=a/b;
+     printf("sum of: %d \n",add);
+     printf("sum of: %d \n",sub);
+     printf("sum of: %d \n",multi);
+     printf("sum of: %d \n",divi);
+
+
+    return 0;
+ }
