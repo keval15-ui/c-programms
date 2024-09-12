9b2eb3682e8894c6d6fdb0499dc2621390b35813
keval15-ui <kevalsolankure@gmail.com>
2024-09-12 10:17:40 +0530

    Create number is even or odd.c

---
+++ number is even or odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+ {
+     int a,b ;
+     printf("Enter value of a:");
+     scanf("%d",&a);
+   
+     
+     if (a % 2 == 0){
+         printf("%d is even \n",a);
+     }
+   else {
+       printf("%d is odd \n",a);
+   }
+ }
+ 
