e4e6bcda33812e12d9a46b88403f3c4f1e7cfb7e
keval15-ui <kevalsolankure@gmail.com>
2024-09-13 22:10:38 +0530

    Create  sum of all digits of a number.c

---
+++ sum of all digits of a number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number, sum = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    number = (number < 0) ? -number : number;
+    while (number > 0) {
+        sum += number % 10;  
+        number /= 10;       
+    }
+    printf("The sum of the digits is: %d\n", sum);
+    
+    return 0;
+}
