e3cd7476f395a549f195749ca6781468e6a60aaa
keval15-ui <kevalsolankure@gmail.com>
2024-10-10 09:50:12 +0530

    Create sum of all even number 1-100.c

---
+++ sum of all even number 1-100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+     int i;
+     printf("sum of all even numbers between 1-100:");
+     for( int i=1;i<=100;i++){
+        if (i%2==0){
+            printf("sum of all even numbers:%d\n",i);
+        }
+     }
+    return 0;
+}
