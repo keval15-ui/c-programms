a88b8c17e8dd39a91166274e39d85d26524817d5
keval15-ui <kevalsolankure@gmail.com>
2024-10-17 09:45:03 +0530

    Create to find the sum of all elements in arry.c

---
+++ to find the sum of all elements in arry.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5,}; 
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i]; 
+    }
+
+    printf("The sum of all elements in the array is %d\n", sum);
+
+    return 0;
+}
